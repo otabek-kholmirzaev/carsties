c9ff4c480a79ee6ebf217cbdfeb9566166557cb2
Otabek Kholmirzaev <kholmirzaevotabek1@gmail.com>
2024-11-28 18:38:51 +0500

    End of section 2

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Entities;
+
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+}
